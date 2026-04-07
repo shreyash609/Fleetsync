de311873c333c1f4bf2eda9ed17957027e7ac2e9
rajat784 <rajatmarsinge803@gmail.com>
2026-04-07 12:44:08 +0530

    Create user Entity and Repository

---
+++ services/auth-service/src/main/java/com/fleetsync/auth_service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fleetsync.auth_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fleetsync.auth_service.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
--- services/auth-service/src/main/java/com/fleetsync/auth_service/service/Test.java
+++ services/auth-service/src/main/java/com/fleetsync/auth_service/service/Test.java
@@ -1,13 +1,47 @@
+//package com.fleetsync.auth_service.service;
+//
+//import com.fleetsync.auth_service.dto.RegisterRequest;
+//
+//public class Test {
+//
+//    public void test(){
+//
+//        RegisterRequest request=new RegisterRequest();
+//
+//    }
+//
+//}
+
 package com.fleetsync.auth_service.service;
 
-import com.fleetsync.auth_service.dto.RegisterRequest;
+import com.fleetsync.auth_service.dto.*;
+import com.fleetsync.auth_service.entity.User;
+import com.fleetsync.auth_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Test {
 
-    public void test(){
+    @Autowired
+    private UserRepository userRepository;
 
-        RegisterRequest request=new RegisterRequest();
+    public AuthResponse register(RegisterRequest request) {
 
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRole(request.getRole());
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .accessToken("dummy-access")
+                .refreshToken("dummy-refresh")
+                .tokenType("Bearer")
+                .name(user.getName())
+                .role(user.getRole())
+                .build();
     }
-
 }
