a3512ff07b7e0abfaad8ff099708a3e783151136
rajat784 <rajatmarsinge803@gmail.com>
2026-04-07 14:16:05 +0530

    relosved

--- services/auth-service/src/main/java/com/fleetsync/auth_service/entity/User.java
+++ services/auth-service/src/main/java/com/fleetsync/auth_service/entity/User.java
@@ -2,6 +2,7 @@ package com.fleetsync.auth_service.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -12,12 +13,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // Auto set createdAt before insert
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
